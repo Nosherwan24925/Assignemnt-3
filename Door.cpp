8cc710f14a106c1e0e27bd755bbcdb8780e90009
Nosherwan <94525538+Nosherwan24925@users.noreply.github.com>
2024-01-28 16:12:52 +0500

    Created Door.cpp

---
+++ Door.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+class Door
+{
+private:
+ bool isOpen;
+ bool isLocked;
+
+public:
+
+void openDoor();
+void closeDoor();
+void lockDoor();
+void unlook();
+bool isDoorOpen();
+bool isDoorLocked();
+};
+
+
+int main()
+{
+
+return 0;
+}
